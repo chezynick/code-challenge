ad3d90407569e6e0000e64a4dedb8df434c04769
chezynick <chezynickannie@yahoo.co.uk>
2023-03-02 14:40:56 +0000

    added full clear functionality

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-const Header = ({setSelectedEmployee}) => {
+import { DATENEWOLD } from '../../Constants';
+const Header = ({ setSelectedEmployee,setSortValue, setAbsencesToDisplay, absences  }) => {
     return ( 
         <div className="w-full h-32 flex justify-between items-center bg-slate-50">
-            <button className='bg-white border border-red-600 rounded-lg px-2 py-1 flex justify-center items-center' onClick={() => setSelectedEmployee()}>Clear selection</button>
+            <button className='bg-white border border-red-600 rounded-lg px-2 py-1 flex justify-center items-center' onClick={() => {
+                setSelectedEmployee();
+                setAbsencesToDisplay(absences);
+                setSortValue(DATENEWOLD)
+            }}>Clear selection</button>
         </div>
      );
 }
